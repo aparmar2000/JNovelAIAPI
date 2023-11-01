e3f7d03fd3fa84b284446391a22ebb45e466510d
aparmar2000 <aparmar2000@gmail.com>
2023-11-01 16:04:24 -0700

    Adjusted AbstractFeatureIntegrationTest to use @BeforeAll instead of @BeforeEach to utilize rate-limiting better.

--- src/test/java/aparmar/nai/AbstractFeatureIntegrationTest.java
+++ src/test/java/aparmar/nai/AbstractFeatureIntegrationTest.java
@@ -1,15 +1,15 @@
 package aparmar.nai;
 
 import org.junit.jupiter.api.Assumptions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 
 @Tag("integration")
 abstract class AbstractFeatureIntegrationTest {
-	protected NAIAPI apiInstance;
+	protected static NAIAPI apiInstance;
 
-	@BeforeEach
-	void setUp() throws Exception {
+	@BeforeAll
+	static void setUp() throws Exception {
 		Assumptions.assumeTrue(TestConstants.getTestAPIKey() != null);
 		apiInstance = new NAIAPI(TestConstants.getTestAPIKey());
 	}
